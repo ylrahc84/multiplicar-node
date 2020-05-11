b643d8dd3399cc082885858c0a799ab9e1a31179
charly <coleccionista84@gmail.com>
2020-05-10 18:40:40 -0600

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,46 @@
+//requireds
+const fs = require('fs'); // es de un proyecto propio de node
+// const fs = require('express'); // eso no es de node es un paquete que se instala
+// const fs = require('./fs');// son propios del proyecto que hemos escrito
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+    console.log('====================='.green);
+    console.log(`==tabla de ${base}===`.green);
+    console.log('====================='.green);
+
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base} * ${i} = ${base * i}`);
+    }
+
+}
+
+let crearArchivo = (base, limite = 10) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El Valor introducido ${base} no es un numero`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += (`${base} * ${i} = ${base * i}\n`);
+        }
+
+        fs.writeFile(`tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
+            if (err)
+                reject(err)
+            else
+                resolve(`tabla-${base}-al-${limite}.txt`);
+        });
+    })
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
